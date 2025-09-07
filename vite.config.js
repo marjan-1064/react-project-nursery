3e405a639b0e097feb0f9480adc801d4ecdbe9ff
Marjan fatima <marjanfatima31@.com>
2025-09-07 21:18:32 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://github.com/marjan-1064/react-project-nursery",
   plugins: [react()],
 })
